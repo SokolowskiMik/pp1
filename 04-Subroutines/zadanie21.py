d7bd53e36904824dce470b1a937babba26feb748
SokolowskiMik <226572@student.uek.krakow.pl>
2022-10-25 16:25:30 +0200

    update

---
+++ 04-Subroutines/zadanie21.py
@@ -0,0 +1,4 @@
+
+test = lambda x, y : True if (x > y) else False
+
+print(test(12, 3))
---
+++ 04-Subroutines/zadanie22.py
@@ -0,0 +1,5 @@
+check = lambda x : True if(x%2 == 0) else False
+
+number = int(input("input a even or odd number: "))
+
+print(check(number))
