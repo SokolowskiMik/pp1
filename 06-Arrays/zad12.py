f88c6652bb19b3f7a1fe0f5bd721705c52f4933d
s-121-23 <s-121-23@gumiforest.ae.krakow.pl>
2022-11-14 14:40:45 +0100

    u

--- 06-Arrays/zad12.py
+++ 06-Arrays/zad12.py
@@ -2,7 +2,23 @@ arr = [[2,5,4],[9,0,3]]
 
 #a
 print(arr)
-print(len(arr[0]) + len(arr[1]))
+#b
+print()
+print((len(arr)), len(arr[0]))
+#c
+print()
 print(arr[0][1])
+#d
+print()
 print(arr[1][2])
+#e
+print()
 print(arr[1])
+#f
+print()
+print(arr[0])
+print(arr[1])
+#g
+print()
+print(arr[0][len(arr[0])-1])
+print(arr[1][len(arr[0])-1])
