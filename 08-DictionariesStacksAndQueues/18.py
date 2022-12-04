e9f6b0b6810123339cd14a351c09380180d7d35d
SokolowskiMik <226572@student.uek.krakow.pl>
2022-12-04 21:28:05 +0100

    u

---
+++ 08-DictionariesStacksAndQueues/18.py
@@ -0,0 +1,18 @@
+import stack
+
+stack.display()
+stack.push(2)
+
+stack.push(14)
+
+stack.push(9)
+stack.display()
+print()
+stack.push(31)
+stack.push(6)
+stack.display()
+print()
+x = stack.pop()
+y = stack.pop()
+print(x, y)
+stack.display()
